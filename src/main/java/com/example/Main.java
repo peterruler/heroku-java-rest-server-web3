9cf52a54869701d940e8b4760ef0d4edc72d4d55
Peter Stroessler <7starch@gmail.com>
2021-11-05 20:00:57 +0100

    test commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -523,7 +523,7 @@ public class Main {
             if (counter > 0) {
                 return json.substring(0, json.length() - 1) + "]";
             } else {
-                return "undefined";
+                return project_id+"undefined";
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
