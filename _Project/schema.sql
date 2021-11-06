4e1e195d5d61b5154bcbae19741ad3f68d14d316
Peter Stroessler <7starch@gmail.com>
2021-11-06 13:17:33 +0100

    add of correct schema

--- _Project/schema.sql
+++ _Project/schema.sql
@@ -2,15 +2,15 @@ set transaction read write;
 DROP TABLE IF EXISTS Project CASCADE;
 DROP TABLE IF EXISTS Issue CASCADE;
 CREATE TABLE Project (
-	id SERIAL PRIMARY KEY,
+	id BIGSERIAL PRIMARY KEY,
 	client_id VARCHAR ( 255 ) NOT NULL,
 	title VARCHAR ( 255 ) NOT NULL,
 	active boolean NOT NULL
 );
 CREATE TABLE Issue (
-	id SERIAL PRIMARY KEY,
+	id BIGSERIAL PRIMARY KEY,
 	client_id VARCHAR ( 255 ) NOT NULL,
-	project_id double precision NOT NULL,
+	project_id BIGINT NOT NULL,
 	done boolean NOT NULL,
 	title VARCHAR ( 255 ) NOT NULL,
 	due_date date NOT NULL,
